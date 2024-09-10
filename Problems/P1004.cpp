4d186bb0f1e87b3481f3ec583626dcd7c1b5e0c6
TianZhu_Wang <stephen_einstein@iCloud.com>
2024-09-10 22:56:56 +0800

    finish P1004

--- Problems/P1004.cpp
+++ Problems/P1004.cpp
@@ -1,21 +1,30 @@
-//
-// Created by 王天柱 on 2024/8/31.
-//
+#include<iostream>
+#include<algorithm>
 
-#include "P1004.h"
-#include <iostream>
 using namespace std;
 
-void P1004() {
-    int n;
+int n,m,map[205][205],f[205][205][205],x,y,v;
+
+void P1004()
+{
     cin >> n;
-    vector<vector<int>> grid(n, vector<int>(n, 0));
+    m = n;
     for (;;) {
         int x, y, v;
         cin >> x >> y >> v;
         if (x == 0 && y == 0 && v == 0) {
             break;
         }
-        grid[x - 1][y - 1] = v;
+        map[x][y] = v;
     }
+    for(int k = 1; k <= m + n; k++)
+        for(int i = 1; i <= min(k,n); i++)
+            for(int j = 1; j <= min(k,n); j++)
+            {
+                f[k][i][j] = max({f[k - 1][i][j],f[k - 1][i - 1][j],f[k - 1][i][j - 1],f[k - 1][i - 1][j - 1]}) + map[k - i + 1][i] + map[k - j + 1][j];
+                if(i == j) f[k][i][j] -= map[k - i + 1][i];
+            }
+
+    f[n + m][n][n] = max({f[n + m - 1][n][n - 1],f[n + m - 1][n - 1][n],f[n + m - 1][n][n],f[n + m - 1][n - 1][n - 1]});
+    printf("%d\n",f[n + m][n][n]);
 }
